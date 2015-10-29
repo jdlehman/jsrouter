4f2eba74fe0124192eaa03fefa1fa701c565e37f
Matt Thompson <mthompson@customink.com>
2015-10-29 16:16:52 -0400

    add lifecycle hooks.
    remove ability to modify route change behavior.

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,30 @@ export function pathFromURL(url) {
     ensureLeadingSlash(url.split('#')[1] || '/')
   );
 }
+
+export function noop() {}
+
+export function recognizeAndCallHandler(path, lastOrNextPath, handlerName) {
+  var pathHandlers = this.recognizer.recognize(path);
+  if (!pathHandlers) {
+    this.unrecognizedRouteHandler(path, lastOrNextPath, handlerName);
+  } else {
+    for (var i = 0; i < pathHandlers.length; i++) {
+      var result = pathHandlers[i];
+      var queryParams = pathHandlers.queryParams;
+      var params = result.params;
+      var handlerObj = this.handlers[result.handler];
+      var handler = handlerObj && handlerObj[handlerName];
+
+      if (typeof handler === 'function') {
+        var handlerArgs = {path, queryParams, params};
+        if (handlerName === 'enter') {
+          handlerArgs.lastPath = lastOrNextPath;
+        } else {
+          handlerArgs.nextPath = lastOrNextPath;
+        }
+        handler(handlerArgs);
+      }
+    }
+  }
+}
